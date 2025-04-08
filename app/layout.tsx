be6e3496d022093ac18d762589bdfdbc5b26e3b8
Ezra003 <126328199+Ezra003@users.noreply.github.com>
2025-04-08 02:01:01 +0200

    improvements trying to deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,16 +44,7 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-          value={{
-            light: "light",
-            dark: "dark",
-          }}
-        >
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           {children}
           <Toaster />
         </ThemeProvider>
